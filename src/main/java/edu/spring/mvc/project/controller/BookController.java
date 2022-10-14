10e60143dbf6808acb293d03bcb5f0510ec5f8d5
efrxngg <efrenpgc2602@gmail.com>
2022-10-14 16:53:00 -0500

    config

--- src/main/java/edu/spring/mvc/project/controller/BookController.java
+++ src/main/java/edu/spring/mvc/project/controller/BookController.java
@@ -3,11 +3,13 @@ package edu.spring.mvc.project.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import edu.spring.mvc.project.domain.entity.Book;
@@ -20,6 +22,7 @@ public class BookController {
     @Autowired
     private IBookService<Book> bookService;
 
+    @ResponseStatus(value = HttpStatus.CREATED)
     @PostMapping(value = "/add")
     public ResponseEntity<Book> add(@RequestBody Book Book) {
         Book result = null;
